95cf888118fd18a2deed44747e3f93800b10e7ab
AlisonLN <alison.nascimento@hotmail.com>
2024-10-16 17:31:35 -0300

    primeiro commmit add projeto springboot

---
+++ src/main/java/com/recipePrice/recipePrice/RecipePriceApplication.java
@@ -0,0 +1,13 @@
+package com.recipePrice.recipePrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipePriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipePriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipePrice/recipePrice/RecipePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipePrice.recipePrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
