9a7d2f6c9fe451c9aa3a1fdf428d30d5889f454a
AlisonLN <alison.nascimento@hotmail.com>
2024-10-25 14:19:41 -0300

    Insercao metodo getAll

--- src/main/java/com/recipePrice/recipePrice/services/ProductService.java
+++ src/main/java/com/recipePrice/recipePrice/services/ProductService.java
@@ -6,6 +6,9 @@ import com.recipePrice.recipePrice.dtos.ProductRequest;
 import com.recipePrice.recipePrice.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
@@ -21,10 +24,16 @@ public class ProductService {
         prod.setPrice(productRequest.price());
         System.out.println("Dados a ser salvo na tabela produto " + prod);
         Product productSaved = productRepository.save(prod);
-        System.out.println("Os dados "+ productSaved + " foram inseridos na tabela produto ");
-        ProductResponse prodResp = new ProductResponse(productSaved.getName(),productSaved.getUnitOfMeasurement(), productSaved.getPrice());
+        System.out.println("Os dados " + productSaved + " foram inseridos na tabela produto ");
+        ProductResponse prodResp = new ProductResponse(productSaved.getName(), productSaved.getUnitOfMeasurement(), productSaved.getPrice());
         System.out.println("Retorno para o Cliente " + prodResp);
         return prodResp;
+
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+        .map(product -> new ProductResponse(product.getName(), product.getUnitOfMeasurement(), product.getPrice())).collect(Collectors.toList());
 
     }
 
